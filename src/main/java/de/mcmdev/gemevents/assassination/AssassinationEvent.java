34a1f70678669a16e8d5dd1b66b8502c86a686f0
MCMDEV <mcmdev@mcmdev.de>
2025-07-27 18:17:30 +0200

    Add reward system to AssassinationEvent with loot table support.

--- src/main/java/de/mcmdev/gemevents/assassination/AssassinationEvent.java
+++ src/main/java/de/mcmdev/gemevents/assassination/AssassinationEvent.java
@@ -11,15 +11,24 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.loot.LootContext;
+import org.bukkit.loot.LootTable;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.Duration;
-import java.util.Objects;
+import java.util.*;
 
 public class AssassinationEvent implements TerminableModule {
+
+    private static final NamespacedKey REWARD_LOOT_TABLE = new NamespacedKey("gemevents", "assassination_event_reward");
+
     @Override
     public void setup(@NotNull TerminableConsumer consumer) {
         Countdown countdown = Countdown.create()
@@ -33,7 +42,7 @@ public class AssassinationEvent implements TerminableModule {
                 .filter(playerDeathEvent -> Objects.nonNull(playerDeathEvent.getEntity().getKiller()))
                 .expireAfter(1)
                 .handler(playerDeathEvent -> {
-                    broadcastWinner(playerDeathEvent.getEntity().getKiller());
+                    win(playerDeathEvent.getEntity().getKiller());
                     countdown.closeAndReportException();
                 })
                 .bindWith(countdown);
@@ -48,10 +57,12 @@ public class AssassinationEvent implements TerminableModule {
         Players.forEach(this::sendExplanation);
     }
 
-    private void broadcastWinner(Player winner) {
+    private void win(Player winner) {
         Players.forEach(player -> {
             player.sendRichMessage("<red><bold><winner></bold> hat das Assassinenevent gewonnen!</red>", Placeholder.component("winner", winner.displayName()));
         });
+
+        winner.give(getRewards(winner), true);
     }
 
     private void sendExplanation(Player player) {
@@ -62,5 +73,19 @@ public class AssassinationEvent implements TerminableModule {
         player.sendMessage(Component.text(" ".repeat(spacers) + title, NamedTextColor.RED, TextDecoration.BOLD));
         player.sendMessage(Component.text("Wer zuerst in den nächsten 60 Minuten einen anderen Spieler besiegt, erhält eine Belohnung.", NamedTextColor.RED));
         player.sendMessage(Component.text(" ".repeat(width), NamedTextColor.RED, TextDecoration.STRIKETHROUGH));
+    }
+
+    private Collection<ItemStack> getRewards(Player player) {
+        LootTable lootTable = Bukkit.getLootTable(REWARD_LOOT_TABLE);
+        if(lootTable == null) {
+            return Set.of(ItemStack.of(Material.ENCHANTED_GOLDEN_APPLE));
+        }
+
+        return lootTable.populateLoot(
+                new Random(),
+                new LootContext.Builder(player.getLocation())
+                        .killer(player)
+                        .build()
+        );
     }
 }
